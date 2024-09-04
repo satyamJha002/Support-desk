ca7dbceae025db578127437d67dec87ae846006d
satyamJha002 <jha.satyam0099@gmail.com>
2024-09-04 12:19:26 +0530

    third commit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const UserModel = require('../model/userModel')
 
 /* @description Register a new user
 @route /api/users
@@ -12,7 +15,38 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new Error('Please include all the fields.')
   }
 
-  res.send('Register route')
+  //Find if user already exist
+  const userExist = await UserModel.findOne({email})
+
+  if(userExist){
+    res.status(400)
+    throw new Error('User already exist!')
+  }
+
+  // hashing password
+  const salt = await bcrypt.genSalt(10)
+
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // create user
+
+  const createUser = await UserModel.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(createUser){
+    res.status(201).json({
+      _id: createUser._id,
+      name: createUser.name,
+      email: createUser.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+
 })
 
 /* @description Login a user
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 3000
@@ -7,6 +9,9 @@ const PORT = process.env.PORT || 3000
 dotenv.config()
 
 const app = express()
+
+// Connect to database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
